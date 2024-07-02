05b00557d13b4ad79e1e164fffe8a2ac5bf34c81
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-07-01 20:44:25 -0400

    setting up views

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Awesome job Glory! This is the index page of the polls application.")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join(q.question_text for q in latest_questions)
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("This is the detail view of the question: %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question: %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Votes on question: %s" % question_id)
