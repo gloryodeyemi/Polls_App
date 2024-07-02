84a20b666728211d87cb721d9ccb9d8bcff798cc
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-07-01 21:04:47 -0400

    Templates

--- polls/views.py
+++ polls/views.py
@@ -6,8 +6,10 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join(q.question_text for q in latest_questions)
-    return HttpResponse(output)
+    context = {
+        'latest_questions': latest_questions
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
